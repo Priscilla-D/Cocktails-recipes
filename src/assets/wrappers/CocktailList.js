1f03762ffb4ff999bffff39d63ff0621bfc715c6
Priscilla-D <durand.priscilla@outlook.fr>
2024-01-04 13:23:50 +0100

    fix mobile screen

--- src/assets/wrappers/CocktailList.js
+++ src/assets/wrappers/CocktailList.js
@@ -1,9 +1,13 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const Wrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
   gap: 2rem;
+
+  @media screen and (max-width: 800px) {
+    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  }
 `;
 
 export default Wrapper;
